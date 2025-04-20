b33d13377e639942e37b6f4981bac25c53dacefa
Somesh Ranjan Biswal <somesh@Sanu.sanu>
2025-04-20 17:02:47 +0530

    poster change

--- js/terms.js
+++ js/terms.js
@@ -9,7 +9,7 @@ let currentCaptcha = '';
 let redirectTimer;
 
 function generateCaptcha() {
-    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const chars = '0123456789';
     let captcha = '';
     for (let i = 0; i < 6; i++) {
         captcha += chars[Math.floor(Math.random() * chars.length)];
